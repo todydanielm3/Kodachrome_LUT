35bc618c34f0069720e03abfb8e71a4ab00bfdd3
DanielMoraes <daniel8moraess@gmail.com>
2025-12-20 20:04:01 -0300

    fix: corrigir entry point do webpack para habilitar upload de imagens
    - Mudança de demo.js para index.js no webpack.config.js
    - Agora o clique para selecionar imagens funciona corretamente

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/demo.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
